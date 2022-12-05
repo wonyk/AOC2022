97a5e21d665e8403e706414c543c83080c0c0161
Won Ying Keat <ykwon.2020@scis.smu.edu.sg>
2022-12-05 19:42:32 +0800

    Added template

---
+++ day5/supplyStacks.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+def part1(inp):
+    p1_result = 0
+
+    return p1_result
+
+
+def part2(inp):
+
+    p2_result = 0
+
+    return p2_result
+
+
+def main():
+    inp = open("input", "r").read().splitlines()
+
+    p1_result = part1(inp)
+    print(f"Part 1: {p1_result}")
+
+    p2_result = part2(inp)
+    print(f"Part 2: {p2_result}")
+
+
+if __name__ == "__main__":
+    main()
